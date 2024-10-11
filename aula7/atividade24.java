eb72f009bccfc6826536d96a49f77b68021a3f89
Joana <jordaojoana20@gmail.com>
2024-10-11 15:34:11 +0100

    atividade 24

---
+++ aula7/atividade24.java
@@ -0,0 +1,17 @@
+public class atividade24 {
+    public static void main(String[] args) {
+        //matriz 3x2
+        int matriz [][] = {{1, 2, 3},{4, 5, 6}};
+
+        for (int i = 0; i < matriz.length; i++){
+            for (int x = 0; x < matriz[i].length; x++){
+                System.out.printf(matriz[i][x] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//matriz transposta 3x2
+
+        
