007b2984b44f94f7a831bfa7d477b4c860d62238
Joana <jordaojoana20@gmail.com>
2024-10-11 16:14:11 +0100

    últimos exercícios

---
+++ aula7/atividade25.java
@@ -0,0 +1,15 @@
+public class atividade25 {
+    public static void main(String[] args) {
+        int total = 0;
+        int count = 0;
+
+        for (int i = 1; i <= 1000; i++){
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.print(i + " ");
+                count++;
+                total += i;
+            }
+        }
+        System.out.println("Total = " + total);
+    }
+}
---
+++ aula7/atividade26.java
@@ -0,0 +1,16 @@
+public class atividade26 {
+    public static void main(String[] args) {
+        mensagem_de_boas_vindas();
+        System.out.println();
+        menu();
+
+    }
+
+    public static void mensagem_de_boas_vindas(){
+        System.out.println("Seja Bem Vindo");
+    }
+
+    public static void menu(){
+        System.out.println("== Menu == \n 1 - Entradas\n 2 - Prato Carne\n 3 - Prato Peixe\n 4 - Sobremesa\n 5 - Sair ");
+    }
+}
