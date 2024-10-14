6c8ad6692323a0e745e6c632e7c519875553861d
Joana <jordaojoana20@gmail.com>
2024-10-14 16:58:23 +0100

    atualização

--- aula7/atividade27.java
+++ aula7/atividade27.java
@@ -2,10 +2,21 @@ import java.util.Scanner;
 
 public class atividade27 {
     public static void main(String[] args) {
-        mensagem_de_boas_vindas();
-        System.out.println();
-        menu();
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Introduza a idade: ");
+        int idade = input.nextInt();
+
+        if (idade >= 18){
+            mensagem_de_boas_vindas();
+            System.out.println();
+            menu_2();
+        } else{
+            mensagem_de_boas_vindas();
+            System.out.println();
+            menu();
+        }
+        input.close();
     }
 
     public static void mensagem_de_boas_vindas(){
@@ -22,6 +33,10 @@ public class atividade27 {
     }
 
     public static void menu(){
-        System.out.println("== Menu == \n 1 - Entradas\n 2 - Prato Carne\n 3 - Prato Peixe\n 4 - Sobremesa\n 5 - Sair ");
+        System.out.println("== Menu == \n 1 - Entradas\n 2 - Prato Carne\n 3 - Prato Peixe\n 4 - Sobremesa\n 5 - Sair");
+    }
+
+    public static void menu_2(){
+        System.out.println("== Menu == \n 1 - Entradas\n 2 - Prato Carne\n 3 - Prato Peixe\n 4 - Sobremesa\n 5 - Carta de Vinhos\n 6 - Sair");
     }
 }
