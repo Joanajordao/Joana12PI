25d7c71726086d1aa477ea1f36e0e5d51668aa34
Joana <jordaojoana20@gmail.com>
2024-10-18 16:00:50 +0100

    atividade32

---
+++ aula10/Atividade32.java
@@ -0,0 +1,18 @@
+package aula10;
+
+public class Atividade32 {
+    public static void main(String[] args) {
+        String idade = "30";
+        
+        //Converção da variável string para int
+        int x = Integer.parseInt(idade);
+
+        //Converção da variável string para float
+        float y = Float.parseFloat(idade);
+
+        System.out.println("Valor int: " + x);
+        System.out.println("Valor float: " + y);
+
+
+    }
+}
