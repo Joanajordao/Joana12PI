2010a2038910d7a78961e7ef5dd69cd2b021cc85
Joana <jordaojoana20@gmail.com>
2024-10-30 14:29:11 +0000

    atualizações

--- aula15/Bolo.java
+++ aula15/Bolo.java
@@ -2,9 +2,33 @@ package aula15;
 //Criação de uma classe que não tem o método main
 public class Bolo {
     //Variáveis de instância
-    public String sabor;
-    public int tamanho;
-    public boolean cobertura;
+    private String sabor;
+    private int tamanho;
+    private boolean cobertura;
+
+    public String getSabor(){
+        return sabor;
+    }
+
+    public void setSabor(String sabor){
+        this.sabor = sabor; // a variável de instância recebe a variável que o utilizador vai inserir o valor
+    }
+
+    public int getTamanho(){
+        return tamanho;
+    }
+
+    public void setTamanho(int tamanho){
+        this.tamanho = tamanho; 
+    }
+
+    public boolean getCobertura(){
+        return cobertura;
+    }
+
+    public void setCobertura(boolean cobertura){
+        this.cobertura = cobertura;
+    }
 
     //Apresenta os detalhes do bolo
     public void exibirDetalhes(){
